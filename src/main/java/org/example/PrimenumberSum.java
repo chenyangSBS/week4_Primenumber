c9b536dec29c26efdf7e1b89aa35497237dd0bc5
Chen Yang <chenyang@CYs-MacBook-Pro.local>
2024-10-23 16:59:11 +0800

    first update

---
+++ src/main/java/org/example/PrimenumberSum.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class PrimenumberSum {
+    public static void main(String[] args) {
+        int left; // 左边界
+        int right; // 有边界
+        Scanner in = new Scanner(System.in);
+        left = in.nextInt();
+        right = in.nextInt();
+        int sum = 0; // 素数和
+        // 你的代码逻辑
+
+        System.out.println(sum);
+    }
+}
